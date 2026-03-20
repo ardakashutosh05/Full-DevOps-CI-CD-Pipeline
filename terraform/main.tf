4cb0e6a0178343f791c4211929f3279268512106
Ashutosh Ardak <ardakashutosh05@gmail.com>
2026-03-20 14:01:24 +0000

    add docker CI pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 resource "aws_instance" "devops" {
   ami           = "ami-0be9cb9f67c8dabd6"
   instance_type = "t3.micro"
+	
+  key_name = "ashu"
 
   tags = {
     Name = "DevOps-Project"
