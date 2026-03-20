9c2f648a857ec220874226195ee70ecabda61a8f
Ashutosh Ardak <ardakashutosh05@gmail.com>
2026-03-20 18:01:17 +0000

    add docker CI pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
-provider aws {
-  region = ap-southeast-1
+provider "aws" {
+  region = "ap-southeast-1"
 }
 
 resource aws_instance devops {
-  ami           = ami-0e7ff22101b84bcff
-  instance_type = t3.micro
+  ami           = "ami-0e7ff22101b84bcff"
+  instance_type = "t3.micro"
 
-  key_name = devops-key
+  key_name = "devops-key"
 
   tags = {
-    Name = DevOps-Project
+    Name = "DevOps-Project"
   }
 }
