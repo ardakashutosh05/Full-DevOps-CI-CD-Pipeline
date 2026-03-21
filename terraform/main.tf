35eb55c6bc647075e95c504f3af5667007de906e
Ashutosh Ardak <ardakashutosh05@gmail.com>
2026-03-21 15:43:06 +0530

    Update AWS instance AMI and key name

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "devops" {
-  ami           = "ami-0be9cb9f67c8dabd6"
+resource aws_instance devops {
+  ami           = "ami-0e7ff22101b84bcff"
   instance_type = "t3.micro"
-	
-  key_name = "ashu"
+
+  key_name = "devops-key"
 
   tags = {
     Name = "DevOps-Project"
